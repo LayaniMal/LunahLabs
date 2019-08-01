a198021e3e182d06e130cd095730cbc34f6af07a
Layani Malsha <layani.malsha@gmail.com>
2019-08-01 23:36:57 +0530

    Add files via upload

---
+++ code1.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>lunah Labs</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style>
+
+   .navbar {
+      margin-bottom: 0;
+      border-radius: 5;
+    }
+
+    footer {
+      background-color: #ffffff;
+      padding: 250px;
+    }
+  .carousel-inner img {
+      
+      width: 2000px;
+      height: 3000px;
+  }
+    .carousel-caption{
+          padding-bottom: 30px;
+          padding-top:500px;
+          padding-left: 100px;
+          padding-right: 100px;
+      }   
+ 
+  @media (max-width: 500px) {
+    .carousel-caption {
+      display:none; 
+    }
+  }
+  
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+    
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+    
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">SERVICES</a></li>
+        <li><a href="#">CAREERS</a></li>
+        <li><a href="#">CONTACT US</a></li> 
+      </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    
+    
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="reativity.png" alt="Image">
+        <div class="carousel-caption">
+          <p>AWAKING CONCEPTS WITH</p>
+    <br>       
+    <font size="5px"><span style="color: aliceblue"><b>L U N A H</span><span style="color: deeppink"> L A B S</b></span></font>
+        </div>      
+      </div>
+
+      <div class="item">
+        <img src="dynamic.png" alt="Image">
+         <div class="carousel-caption">
+        <p>AWAKING CONCEPTS WITH</p>
+      <br>
+       <font size="5px"><span style="color: aliceblue"><b>L U N A H</span><span style="color: deeppink"> L A B S</b></span></font>
+        </div>      
+      </div>
+      
+      <div class="item">
+        <img src="innovation.png" alt="Image">
+         <div class="carousel-caption">
+        <p>AWAKING CONCEPTS WITH</p>
+      <br>
+       <font size="5px"><span style="color: aliceblue"><b>L U N A H</span><span style="color: deeppink"> L A B S</b></span></font>
+        </div>      
+      </div>
+    
+    <div class="item">
+        <img src="determination.png" alt="Image">
+         <div class="carousel-caption">
+        <p>AWAKING CONCEPTS WITH</p>
+      <br>
+       <font size="5px"><span style="color: aliceblue"><b>L U N A H</span><span style="color: deeppink"> L A B S</b></span></font>
+        </div>      
+      </div>
+      
+    </div>
+
+
+	 <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    
+      	<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+   
+   
+   
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div>
+
+ 
+<div class="container text-left">    
+ 
+
+ <font face="Modern,Arial" size="4"><br><br>A B O U T &nbsp; L U N A H &nbsp; L A B S</font><br><br><br>
+  <div class="row">
+      
+    <div class="col-sm-4">
+        <p><font face="Roboto,sans-serif" size="5" color="#a6acaf"> "Creativity is not the find of a "thing", but the making "something" out of it after it is found."</font>
+        </p>
+    
+        <font face="Roboto,sans-serif" size="2" color="#a6acaf"> -JAMES RUSSELL LOWELL</font>
+
+    </div>
+    <div class="col-sm-4">
+      <p>
+       <font face="Roboto,sans-serif" size="3" color="#a6acaf">From dreams to reality, empowering and redefining. We at Lunah Labs, add a creative value to your story whilst innovating with a passion to build everlasting partnerships. We understand the struggle and process that takes place when building your presence, creating exciting content and driving engagement for your business. This is why we love building partnerships to grow with you.</font></p> 
+ 
+    </div>
+
+<div class="col-sm-4">
+      <div class="">
+          <ul>
+           <font face="Roboto,sans-serif" size="3" color="#a6acaf">
+          <li>We’re intimate listeners.</li><br>
+          <li>We’re zealous investigators.</li><br>
+          <li>We look for what works and then we make it seamlessly work for you</li><br>
+          </font>
+          </ul>
+      </div>
+    </div>
+  </div>
+  
+  
+  <div id="digital-team">
+        <br>
+        <center><font size="4">T H E &nbsp; D I G I T A L &nbsp; T E A M</font></center><br><br> 
+      
+        <p>We are an elite squad of misfits, wizards, geeks, wordsmiths, marketers, strategists and space-age thinkers. We are grounded and steered by people with insatiable appetites for innovation, humble spirits and creatively-weird souls. We don’t claim to be unflawed. Yet we strive to deliver a flawless performance in everything we do for you. </p>
+      </div>
+    <div id="iodelogy">
+        <br><br><br>
+        <center>I D E O L O G Y </center><br><br>
+        <div class="col-sm-4">
+            <center> <img src="two-overlapping-speech-bubbles_318-36307.jpg" width=20% height="20%"><br><br>
+            D R E A M<br></center><br>
+            <p><font color="#a6acaf">>We dream, think and plan. Our dedicated team at Lunah Lab creates detailed specifications to cater to your requirements</font></p> <br><br><br>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="images.png" width=20% height="20%"><br><br>
+                B U I L D<br></center><br>
+            <p><font color="#a6acaf">We create, we transform, and we build. From the initial stages of any of our projects to the final letter of a code, we ensure passion driven precision. </font></p> <br><br><br>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="clock-michael-breuer-03.png" width=20% height="20%"><br><br>
+                D L I V E R<br></center><br>
+            <p><font color="#a6acaf">Quality, usability and adaptability. We are equipped with the three –ties that a successful project needs so that it can deliver a return to your business.</font> </p> <br><br><br>
+        </div>
+    </div>
+    
+    
+        <br><br><br>
+        
+        <div class="col-sm-4">
+            <center> <img src="newspaper.png" width=20% height="20%"><br><br>
+            P R E S E N C E<br></center><br>
+            <p><font color="#a6acaf">Being present and making your mark in the digital age is an essential part for any and every business.</font> <br><br><br></p>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="monitor.png" width=20% height="20%"><br><br>
+                C O N T E N T<br></center><br>
+            <p><font color="#a6acaf">Relevance, design and vibrancy is key when it comes to content creation so that you could get past your competitors and get the right return. </font> <br><br><br></p>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="flag.png" width=20% height="20%"><br><br>
+               E N G A G E M E N T<br></center><br>
+            <p><font color="#a6acaf">Quality, usability and adaptability. We are equipped with the three –ties that a successful project needs so that it can deliver a return to your business. </font></p>
+            <br><br><br>
+        </div>
+   
+    <div>
+        
+   <center><br><br><br>W H Y &nbsp; C H O O S E &nbsp; U S</center><br><br>
+        <p> We are obsessed with you: your clients, your peers, your likes, your dislikes, your ambitions and your rivals. While remaining in perpetual learning mode, we also continually aspire to learn more about who you are and how we can grow with you. Allow us to blend brand science with unconventional digital wisdom to amaze you with our wizardry.<br><br><br>
+</p>
+    </div >
+        <div class="col-sm-4">
+            <center> <img src="puzzal.png" width=20% height="20%"><br><br>
+           W E ' R E &nbsp; C R E A T I V E<br></center><br>
+            <p>When you love what you do, creativity flows. We don't need to try, we just do.  <br><br><br></p>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="icons8-knight-50.png" width=20% height="20%"><br><br>
+                W E ' R E &nbsp; P U N C T U A L<br></center><br>
+            <p>No.1 rule for any leader is punctuality. That's why we're leaders.  <br><br><br></p>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="atom.png" width=20% height="20%"><br><br>
+               W E &nbsp; H A V E &nbsp; M A G I C<br></center><br>
+            <p>"Every time we touch that track it turns into gold. Everybody knows we got the magic in us"   </p>
+            <br><br><br>
+        </div>
+        <div class="col-sm-6">
+            <center> <img src="glass.png" width=10% height="10%"><br><br>
+                W E ' R E &nbsp; P U N C T U A L<br></center><br>
+            <p>No.1 rule for any leader is punctuality. That's why we're leaders.  <br><br><br></p>
+        </div>
+        <div class="col-sm-6">
+            <center> <img src="bulb.png" width=10% height="10%"><br><br>
+               W E &nbsp; E M P O W E R<br></center><br>
+            <p>Re-creating experiences for the benefit of you and your customers is exactly why we do, what we do.  </p>
+            <br><br><br>
+        </div>
+   
+        
+    
+     <div id="digital">
+        <br><br><br>
+        <center>D I G I T A L S E R V I C E</center><br><br>
+        <div class="col-sm-4">
+            <center> <img src="development.jpg" width=90% height="90%"><br><br>
+            </center><br>
+             <br><br><br>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="design.jpg" width=90% height="90%"><br><br>
+                </center><br>
+            <br><br><br>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="digital.jpg" width=90% height="90%"><br><br>
+                </center><br>
+             <br><br><br>
+        </div>
+    </div>
+    
+    
+        <br><br><br> <br><br><br> <br><br><br>
+        
+<br>
+
+<div class="container text-center">    
+  <h3>What We Do</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Current Project</p>
+    </div>
+    <div class="col-sm-4"> 
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Project 2</p>    
+    </div>
+    <div class="col-sm-4">
+      <div class="well">
+       <p>Some text..</p>
+      </div>
+      <div class="well">
+       <p>Some text..</p>
+      </div>
+    </div>
+  </div>
+</div><br>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
+
+</body>
+</html>
+
---
+++ lunah.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Lunah Labs</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style>
+  
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 10;
+    }
+
+    footer {
+      background-color: #ffffff;
+      padding: 250px;
+    }
+  .carousel-inner img {
+      
+      width: 1500px;
+      height: 200px;
+  }
+    .carousel-caption{
+          padding-bottom: 320px;
+          padding-top: 20px;
+          padding-left: 100px;
+          padding-right: 100px;
+      }   
+ 
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display:none; 
+    }
+  }
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header" >
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">SERVICES</a></li>
+        <li><a href="#">CAREERS</a></li>
+        <li><a href="#">CONTACT US</a></li> 
+          
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="reativity.png" alt="Image">
+        <div class="carousel-caption">
+            <p>AWAKING CONCEPTS WITH</p>
+            <br>
+            <font size="40"><span style="color: aliceblue">L U N A H</span><span style="color: deeppink">  L A B S</span></font>
+        </div>      
+      </div>
+
+      <div class="item">
+        <img src="dynamic.png" alt="Image">
+        <div class="h-100 row align-items-center">
+          <p>AWAKING CONCEPTS WITH</p>
+            <br>
+            <font size="10px"><span style="color: aliceblue">L U N A H</span><span style="color: deeppink">  L A B S</span></font>
+        </div>      
+      </div>
+    </div>
+    
+    
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+   
+</div>
+  
+<div class="container text-left">    
+  <h3>A B O U T L U N A H L A B S</h3><br>
+  <div class="row">
+      
+    <div class="col-sm-4">
+        <p><h3> "Creativity is not the find of a "thing", but the making "something" out of it after it is found."</h3><br>
+        - JAMES RUSSELL LOWELL
+      <!--img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Current Project</p-->
+    </div>
+    <div class="col-sm-4">
+        <p>From dreams to reality, empowering and redefining. We at Lunah Labs, add a creative value to your story whilst innovating with a passion to build everlasting partnerships. We understand the struggle and process that takes place when building your presence, creating exciting content and driving engagement for your business. This is why we love building partnerships to grow with you.</p> 
+    </div>
+    <div class="col-sm-4">
+      <div class="">
+          <ul>
+          <li>We’re intimate listeners.</li>
+          <li>We’re zealous investigators.</li>
+          <li>We look for what works and then we make it seamlessly work for you</li>
+          </ul>
+      </div>
+    </div>
+  </div>
+    <div id="digital-team">
+        <br>
+        <center> T H E &nbsp; D I G I T A L &nbsp; T E A M</center><br><br> 
+      <!--img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"-->
+        <p>We are an elite squad of misfits, wizards, geeks, wordsmiths, marketers, strategists and space-age thinkers. We are grounded and steered by people with insatiable appetites for innovation, humble spirits and creatively-weird souls. We don’t claim to be unflawed. Yet we strive to deliver a flawless performance in everything we do for you. </p>
+      </div>
+    <div id="iodelogy">
+        <br><br><br>
+        <center>I D E O L O G Y </center><br><br>
+        <div class="col-sm-4">
+            <center> <img src="two-overlapping-speech-bubbles_318-36307.jpg" width=20% height="20%"><br><br>
+            D R E A M<br></center><br>
+            <p>We dream, think and plan. Our dedicated team at Lunah Lab creates detailed specifications to cater to your requirements</p> <br><br><br>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="images.png" width=20% height="20%"><br><br>
+                B U I L D<br></center><br>
+            <p>We create, we transform, and we build. From the initial stages of any of our projects to the final letter of a code, we ensure passion driven precision. </p> <br><br><br>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="clock-michael-breuer-03.png" width=20% height="20%"><br><br>
+                D L I V E R<br></center><br>
+            <p>Quality, usability and adaptability. We are equipped with the three –ties that a successful project needs so that it can deliver a return to your business. </p> <br><br><br>
+        </div>
+    </div>
+    
+    
+        <br><br><br> <br><br><br> <br><br><br>
+        
+        <div class="col-sm-4">
+            <center> <img src="newspaper.png" width=20% height="20%"><br><br>
+            P R E S E N C E<br></center><br>
+            <p>Being present and making your mark in the digital age is an essential part for any and every business. <br><br><br></p>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="monitor.png" width=20% height="20%"><br><br>
+                C O N T E N T<br></center><br>
+            <p>Relevance, design and vibrancy is key when it comes to content creation so that you could get past your competitors and get the right return.  <br><br><br></p>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="flag.png" width=20% height="20%"><br><br>
+               E N G A G E M E N T<br></center><br>
+            <p>Quality, usability and adaptability. We are equipped with the three –ties that a successful project needs so that it can deliver a return to your business. </p>
+            <br><br><br>
+        </div>
+   
+    <div>
+        
+       <center><br><br><br>W H Y &nbsp; C H O O S E &nbsp; U S</center><br><br>
+        <p> We are obsessed with you: your clients, your peers, your likes, your dislikes, your ambitions and your rivals. While remaining in perpetual learning mode, we also continually aspire to learn more about who you are and how we can grow with you. Allow us to blend brand science with unconventional digital wisdom to amaze you with our wizardry.<br><br><br>
+</p>
+    </div >
+        <div class="col-sm-4">
+            <center> <img src="puzzal.png" width=20% height="20%"><br><br>
+           W E ' R E &nbsp; C R E A T I V E<br></center><br>
+            <p>When you love what you do, creativity flows. We don't need to try, we just do.  <br><br><br></p>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="icons8-knight-50.png" width=20% height="20%"><br><br>
+                W E ' R E &nbsp; P U N C T U A L<br></center><br>
+            <p>No.1 rule for any leader is punctuality. That's why we're leaders.  <br><br><br></p>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="atom.png" width=20% height="20%"><br><br>
+               W E &nbsp; H A V E &nbsp; M A G I C<br></center><br>
+            <p>"Every time we touch that track it turns into gold. Everybody knows we got the magic in us"   </p>
+            <br><br><br>
+        </div>
+        <div class="col-sm-6">
+            <center> <img src="glass.png" width=10% height="10%"><br><br>
+                W E ' R E &nbsp; P U N C T U A L<br></center><br>
+            <p>No.1 rule for any leader is punctuality. That's why we're leaders.  <br><br><br></p>
+        </div>
+        <div class="col-sm-6">
+            <center> <img src="bulb.png" width=10% height="10%"><br><br>
+               W E &nbsp; E M P O W E R<br></center><br>
+            <p>Re-creating experiences for the benefit of you and your customers is exactly why we do, what we do.  </p>
+            <br><br><br>
+        </div>
+   
+        
+    
+     <div id="digital">
+        <br><br><br>
+        <center>D I G I T A L S E R V I C E</center><br><br>
+        <div class="col-sm-4">
+            <center> <img src="development.jpg" width=90% height="90%"><br><br>
+            </center><br>
+             <br><br><br>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="design.jpg" width=90% height="90%"><br><br>
+                </center><br>
+            <br><br><br>
+        </div>
+        <div class="col-sm-4">
+            <center> <img src="digital.jpg" width=90% height="90%"><br><br>
+                </center><br>
+             <br><br><br>
+        </div>
+    </div>
+    
+    
+        <br><br><br> <br><br><br> <br><br><br>
+        
+<br>
+
+<footer class="container-fluid text-center">
+    <p>&copy; L &nbsp;  U &nbsp; N&nbsp;  A &nbsp; H &nbsp; L &nbsp; A &nbsp; B &nbsp; S.</p>
+    <P> Lets create the future while we innovate our present<br>Privacy Policy</P>
+</footer>
+    </body>
+    
+    <div class="contact">
+  <h4 class="page-header" style="text-align: center;">CONTACT</h4>
+                <form role="form" action="first.php" method="POST">
+
+                    <div class="form-group float-label-control">
+                       
+                        <input type="text" class="form-control" name="name"
+                        placeholder="NAME">
+                    </div>
+                    <div class="form-group float-label-control">
+                       
+                        <input type="email" class="form-control" name="email" placeholder="EMAIL">
+                    </div>
+                    <div class="form-group float-label-control">
+
+                        <input type="text" class="form-control" name="phone" placeholder="PHONE NUMBER">
+                    </div>
+                    <div class="form-group float-label-control">
+                      
+                        <textarea class="form-control" placeholder="MESSAGE" rows="1" name="message"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn-btn-primary-btn-md">SUBMIT MESSAGE</button>
+                </form>
+
+</div>
+   
+<?php
+      if(isset($_POST['name'])){
+        $name=$_POST["name"];
+      }
+      if(isset($_POST['email'])){
+        $email=$_POST["email"];
+      }
+      if(isset($_POST['phone'])){
+        $phone=$_POST["phone"];
+      }
+      if(isset($_POST['message'])){
+        $message=$_POST["message"];
+      }
+      
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+
+      // Create connection
+      $con = new mysqli($servername, $username, $password);
+
+      if ($con->connect_error) {
+        die(" " . $conn->connect_error);
+      }
+      if(mysqli_query($con,"CREATE DATABASE UserInfo")){
+          echo "Database created...";
+      }
+            
+      mysqli_select_db($con,"UserInfo");
+      mysqli_query($con,"CREATE TABLE dataList(Name varchar(20),Email varchar(20),PhoneNumber int(20),Message varchar(40))");
+
+      
+        mysqli_query($con,"INSERT INTO dataList VALUES('$name','$email','$phone','$message')");
+          
+      
+      
+    ?>
+ </html>
